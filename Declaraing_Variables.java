9463c55871c54292e561e5f3c2b815e131949b7b
PRASAD R IYENGAR <89585155+Prasad20015@users.noreply.github.com>
2021-08-28 14:58:49 +0530

    Declaraing_Variables.java

---
+++ Declaraing_Variables.java
@@ -0,0 +1,13 @@
+
+public class Declaraing_Variables {
+
+	public static void main(String[] args) {
+	int number = 5;
+	float floatnumber = 5.99f;
+	char character ='P';
+	boolean booleanval = true;
+	String name = "Prasad";
+
+	}
+
+}
