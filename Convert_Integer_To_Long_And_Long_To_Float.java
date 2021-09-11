26dcaa4e413ea96753f6eb80d1f5f167f4425126
PRASAD R IYENGAR <89585155+Prasad20015@users.noreply.github.com>
2021-09-11 19:56:21 +0530

    Convert_Integer_To_Long_And_Long_To_Float.java

---
+++ Convert_Integer_To_Long_And_Long_To_Float.java
@@ -0,0 +1,14 @@
+
+public class Convert_Integer_To_Long_And_Long_To_Float {
+
+	public static void main(String[] args) {
+		int i=900;  
+		long l=30;
+		float F=20;
+		System.out.println(" Rocket :" +i );
+		System.out.println(" Word :" +l );
+		System.out.println(" God :" +F );
+
+	}
+
+}
